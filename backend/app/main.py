c13248b0f7721551e6ef2623c1c625dd4fab9dd1
Kruthika G <kruthikagowdagg@gmail.com>
2026-01-12 17:30:07 +0530

    Add full backend main logic

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,57 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from fastapi import UploadFile, File
+from .ocr import extract_text_from_image
+
+from .database import SessionLocal, engine, Base
+from .models import User
+from .schemas import UserRegister, UserLogin
+from .auth import hash_password, verify_password
+
+# CREATE TABLES (CORRECT)
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def root():
+    return {"message": "Backend working"}
+
+@app.post("/auth/register")
+def register(user: UserRegister, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.username == user.username).first():
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+
+    return {"message": "User registered"}
+
+@app.post("/auth/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == user.username).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    return {"message": "Login success"}
+
+@app.post("/ocr/extract-text")
+def extract_text(file: UploadFile = File(...)):
+    text = extract_text_from_image(file)
+    return {
+        "extracted_text": text
+    }
 
